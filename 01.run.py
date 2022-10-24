6286823e102536764e96615105129711c3e15ff1
IvanaXu <1440420407@qq.com>
2022-10-25 04:34:03 +0800

    Update 01.run.py
    print("END", n)

--- 01.run.py
+++ 01.run.py
@@ -1,37 +1,45 @@
 # python 3.10
+n = 0
+
 try:
     import pandas as pd
     print(pd)
 except:
+    n += 1
     print("ERROR pd")
 
 try:
     import numpy as np
     print(np)
 except:
+    n += 1
     print("ERROR np")
 
 try:
     from tqdm import tqdm
     print(tqdm)
 except:
+    n += 1
     print("ERROR tqdm")
 
 try:
     import sklearn
     print(sklearn)
 except:
+    n += 1
     print("ERROR sklearn")
 
 try:
     import jieba
     print(jieba)
 except:
+    n += 1
     print("ERROR jieba")
 
 try:
     import gensim
     print(gensim)
 except:
+    n += 1
     print("ERROR gensim")
-print()
+print("END", n)
